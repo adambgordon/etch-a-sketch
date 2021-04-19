43630667b177dc1f56de61eab0eea785460ba3c1
Adam Gordon <adambaumgordon@gmail.com>
2021-04-19 13:10:26 -0600

    update

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 // Main code
-let gridSize = 32;
+let gridSize;
+initGrid();
 
 const slider = getSliderElement();
-slider.addEventListener("input",updateSliderOutput);
+slider.addEventListener("input",sliderMove);
 slider.addEventListener("change",resetGrid);
 
 // Gets slider element
@@ -10,10 +11,14 @@ function getSliderElement() {
     return document.querySelector(".slider");
 }
 
+// Called whenever slider moves
+function sliderMove(event) {
+    updateSliderOutput(this.value);
+}
+
 // Sets output, e.g. "32 x 32"
-function updateSliderOutput(event) {
-    gridSize = this.value;
-    const output = gridSize + " x " + gridSize;
+function updateSliderOutput(sizeValue) {
+    const output = sizeValue + " x " + sizeValue;
     document.querySelector(".slider-output").textContent = output;
 }
 
@@ -22,19 +27,47 @@ function getCanvasElement() {
     return document.querySelector(".canvas");
 }
 
-// Builds grid based on a grid size
+// Resets the grid when the slider is released on a value
 function resetGrid(event) {
     gridSize = this.value;
+    clearGrid();
+    buildGrid();
+}
+
+function clearGrid () {
     const canvas = getCanvasElement();
-    const gridUnitDimension = Math.floor(500/gridSize);
+    while (canvas.firstChild) {
+        canvas.removeChild(canvas.lastChild);
+    }
+}
+
+// Initializes grid to 32x32
+function initGrid() {
+    gridSize = 32;
+    updateSliderOutput(gridSize);
+    buildGrid();
+}
+
+// Builds grid
+function buildGrid() {
+    const canvas = getCanvasElement();
+    let gap = parseInt(getComputedStyle(canvas).getPropertyValue("row-gap"),10);
+    let gridUnitDimension = Math.floor(500/gridSize) - gap;
+
+    let canvasDimension = gridSize * (gridUnitDimension + gap) - 1;
+
+    console.log(canvasDimension);
 
     canvas.setAttribute("style",
     `grid-template-columns: repeat(${gridSize},${gridUnitDimension}px);
     grid-template-rows: repeat(${gridSize},${gridUnitDimension}px);
-    width: ${gridSize * gridUnitDimension};
-    height: ${gridSize * gridUnitDimension};`);
+    width: ${canvasDimension}px;
+    height: ${canvasDimension}px;`);
 
-    for (let i = 0; i < gridSize * gridSize; i++) {
-        canvas.appendChild(document.createElement("div"));
+
+    for (let i = 0; i < gridSize**2; i++) {
+        let newBox = document.createElement("div");
+        newBox.classList.add("box");
+        canvas.appendChild(newBox);
     }
 }
