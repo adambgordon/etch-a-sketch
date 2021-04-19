4d5a05601a780a9caca83395f9bfe57bafadb886
Adam Gordon <adambaumgordon@gmail.com>
2021-04-19 12:09:47 -0600

    udpate

--- script.js
+++ script.js
@@ -1,25 +1,38 @@
-buildGrid();
+// Main code
+let gridSize = 32;
 
+const slider = getSliderElement();
+slider.addEventListener("input",updateSliderOutput);
+slider.addEventListener("change",resetGrid);
 
-// Returns grid size element
-function getGridSizeElement() {
-    return document.querySelector(".grid-size");
+// Gets slider element
+function getSliderElement() {
+    return document.querySelector(".slider");
 }
 
+// Sets output, e.g. "32 x 32"
+function updateSliderOutput(event) {
+    gridSize = this.value;
+    const output = gridSize + " x " + gridSize;
+    document.querySelector(".slider-output").textContent = output;
+}
+
+// Returns canvas grid element
 function getCanvasElement() {
     return document.querySelector(".canvas");
 }
 
-
-function buildGrid() {
+// Builds grid based on a grid size
+function resetGrid(event) {
+    gridSize = this.value;
     const canvas = getCanvasElement();
+    const gridUnitDimension = Math.floor(500/gridSize);
 
-    const gridSize = getGridSizeElement().textContent;
-    const gridUnitDimension = Math.round(500/gridSize);
-    console.log(gridUnitDimension);
     canvas.setAttribute("style",
     `grid-template-columns: repeat(${gridSize},${gridUnitDimension}px);
-    grid-template-rows: repeat(${gridSize},${gridUnitDimension}px);`);
+    grid-template-rows: repeat(${gridSize},${gridUnitDimension}px);
+    width: ${gridSize * gridUnitDimension};
+    height: ${gridSize * gridUnitDimension};`);
 
     for (let i = 0; i < gridSize * gridSize; i++) {
         canvas.appendChild(document.createElement("div"));
