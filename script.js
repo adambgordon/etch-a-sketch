821465f61e2dfd4cde262931a8456db25e0c61e0
Adam Gordon <adambaumgordon@gmail.com>
2021-04-19 18:51:07 -0600

    update

--- script.js
+++ script.js
@@ -1,69 +1,160 @@
-// Main code
+/* Main code */
+
 let gridSize;
+let shadeColor;
+let schemeColor;
+initMenu();
 initGrid();
-initSlider();
 
+/* Functions */
 
+// Initializes all menu items by adding listeners and setting initial values
+function initMenu () {
+    initSlider();
+    initColorPicker();
+    initRandomColorButton();
+    initRainbowButton();
+    initColorSchemeButton();
+    initEraserButton();
+    initGridlinesButton();
+    initClearButton();
+}
 
-
-// Adds the slider listeners
+/* Menu init functions */
 function initSlider() {
     const slider = getSliderElement();
-    slider.addEventListener("input",sliderMove);
-    slider.addEventListener("change",resetGrid);
+    slider.addEventListener("input",sliderMove); // Updates string in real time
+    slider.addEventListener("change",resetGrid); // Updates actual grid only when cursor releases
 }
 
-function startShading(event) {
-    const boxes = document.querySelectorAll(".box");
+function initColorPicker() {
+    const colorPicker = document.querySelector(".color-picker");
+    colorPicker.addEventListener("change",updateShadeColor);
+    setRandomColor();
+}
+
+function initRandomColorButton() {
+    const randomColorButton = document.querySelector(".random-color-button");
+    randomColorButton.addEventListener("click",setRandomColor);
+}
+
+function initRainbowButton() {
+    const rainbowButton = document.querySelector(".rainbow-button");
+    rainbowButton.addEventListener("click",toggleRainbowMode);
+}
+
+function initColorSchemeButton() {
+    const colorSchemeButton = document.querySelector(".color-scheme-button");
+    colorSchemeButton.addEventListener("click",toggleColorSchemeMode);
+}
+
+function initEraserButton () {
+    const eraserButton = document.querySelector(".eraser-button");
+    eraserButton.addEventListener("click",toggleEraser);
+}
+
+function initClearButton () {
+    const clearButton = document.querySelector(".clear-button");
+    clearButton.addEventListener("click",clearAllShading);
+}
+
+function initGridlinesButton () {
+    const gridlinesButton = document.querySelector(".gridlines-button");
+    gridlinesButton.addEventListener("click",toggleGridlines);
+}
+
+/* Color manipulation functions */
+function setRandomColor() {
+    const colorPicker = document.querySelector(".color-picker");
+    shadeColor = randomHexColor();
+    colorPicker.setAttribute("value",shadeColor);
+    schemeColor = shadeColor;
+}
+
+function randomHexColor() {
+    let hexColor = "#";
+    for (let i = 0; i < 6; i++) {
+        hexColor += Math.floor(Math.random()*16).toString(16);
+    }
+    return hexColor;
+}
+
+function updateShadeColor () {
+    shadeColor = this.value;
+    schemeColor = shadeColor;
+}
+
+/* Slider manipulation functions */
+function getSliderElement() {
+    return document.querySelector(".slider");
+}
+
+function sliderMove() {
+    updateSliderOutput(this.value);
+}
+
+function updateSliderOutput(sizeValue) {
+    const output = sizeValue + " x " + sizeValue;
+    document.querySelector(".slider-output").textContent = output;
+}
+
+/* Box shading functions */
+function startShading() {
+    const boxes = getBoxesList();
     boxes.forEach(box => box.addEventListener("mouseover",addShading));
-    this.classList.add("shading"); // Adds shading to first box clicked (otherwise only starts with next box)
+    let currentBoxColor = getCurrentColor();
+    this.setAttribute("style",`background-color: ${currentBoxColor};`); // Adds shading to first box clicked (otherwise only starts with next box)
 }
 
-function addShading(event) {
-    this.classList.add("shading");
+function addShading() {
+    let currentBoxColor = getCurrentColor();
+    this.setAttribute("style",`background-color: ${currentBoxColor};`);
 }
 
-function stopShading(event) {
-    const boxes = document.querySelectorAll(".box");
+function getCurrentColor() {
+    if (document.querySelector(".eraser-on") !== null) {
+        return "#FFFFFF";
+    } else if (document.querySelector(".color-scheme-on") !== null) {
+        return shiftColor(schemeColor);
+    } else if (document.querySelector(".rainbow-on") !== null) {
+        return randomHexColor();
+    } else {
+        return shadeColor;
+    }
+}
+
+function stopShading() {
+    const boxes = getBoxesList();
     boxes.forEach(box => box.removeEventListener("mouseover",addShading));
 }
 
 function addBoxListeners() {
-    const boxes = document.querySelectorAll(".box");
+    const boxes = getBoxesList();
     boxes.forEach(box => box.addEventListener("mouseup",stopShading));
     boxes.forEach(box => box.addEventListener("mousedown",startShading));
 }
 
 function clearAllShading() {
-
+    const boxes = getBoxesList();
+    boxes.forEach(box => box.setAttribute("style","background-color: white;"));
 }
 
-// Gets slider element
-function getSliderElement() {
-    return document.querySelector(".slider");
+function getBoxesList() {
+    return document.querySelectorAll(".box");
 }
 
-// Called whenever slider moves
-function sliderMove(event) {
-    updateSliderOutput(this.value);
-}
 
-// Sets output, e.g. "32 x 32"
-function updateSliderOutput(sizeValue) {
-    const output = sizeValue + " x " + sizeValue;
-    document.querySelector(".slider-output").textContent = output;
-}
 
 // Returns canvas grid element
 function getCanvasElement() {
     return document.querySelector(".canvas");
 }
 
-// Resets the grid when the slider is released on a value
-function resetGrid(event) {
+/* Grid manipulation functions */
+function resetGrid() {
     gridSize = this.value;
     deleteGrid();
-    buildGrid();
+    buildGrid(getGapValue());
 }
 
 // Deletes all box elements from the DOM
@@ -74,33 +165,146 @@ function deleteGrid () {
     }
 }
 
-// Initializes grid to 32x32
+// Initializes grid to 32x32 with 1px gap for gridlines
 function initGrid() {
-    gridSize = 32;
+    gridSize = 50;
+    const gap = 1;
     updateSliderOutput(gridSize);
-    buildGrid();
+    buildGrid(gap);
 }
 
-// Builds grid by adding box elements
+// Builds grid by creating and appending box elements
 function buildGrid() {
-    const canvas = getCanvasElement();
-
-    let gap = parseInt(getComputedStyle(canvas).getPropertyValue("row-gap"),10);
-    let gridUnitDimension = Math.floor(512/gridSize) - gap;
-    let canvasDimension = gridSize * (gridUnitDimension + gap) - 1;
-
-    canvas.setAttribute("style",
-    `grid-template-columns: repeat(${gridSize},${gridUnitDimension}px);
-    grid-template-rows: repeat(${gridSize},${gridUnitDimension}px);
-    width: ${canvasDimension}px;
-    height: ${canvasDimension}px;`);
-
-
+    setGridDimensions(getGapValue());
     for (let i = 0; i < gridSize**2; i++) {
         let newBox = document.createElement("div");
         newBox.classList.add("box");
         canvas.appendChild(newBox);
     }
-
     addBoxListeners();
+}
+
+function getGapValue () {
+    canvas = getCanvasElement();
+    return parseInt(getComputedStyle(canvas).getPropertyValue("row-gap"),10);
+}
+
+
+function toggleGridlines() {
+    let gap = getGapValue() === 1 ? 0 : 1;
+    setGridDimensions(gap);
+}
+
+function setGridDimensions(gap) {
+    const canvas = getCanvasElement();
+    let gridUnitDimension = Math.floor(512/gridSize) - gap;
+    let canvasDimension = gridSize * (gridUnitDimension + gap) - gap;
+
+    canvas.setAttribute("style",
+    `grid-template-columns: repeat(${gridSize},${gridUnitDimension}px);
+    grid-template-rows: repeat(${gridSize},${gridUnitDimension}px);
+    width: ${canvasDimension}px;
+    height: ${canvasDimension}px;
+    row-gap: ${gap}px;
+    column-gap: ${gap}px;`);
+}
+
+/* Eraser functions */
+function toggleEraser() {
+    const eraserOn = "eraser-on";
+
+    const eraserButton = document.querySelector(".eraser-button");
+    
+    if (eraserButton.classList.contains(eraserOn)) {
+        eraserButton.classList.remove(eraserOn);
+    } else {
+        eraserButton.classList.add(eraserOn);
+    }
+}
+
+function toggleRainbowMode () {
+    const rainbowOn = "rainbow-on";
+    const colorSchemeOn = "color-scheme-on";
+
+    const rainbowButton = document.querySelector(".rainbow-button");
+    const colorSchemeButton = document.querySelector(".color-scheme-button");
+
+    if (rainbowButton.classList.contains(rainbowOn)) {
+        rainbowButton.classList.remove(rainbowOn);
+    } else {
+        if(colorSchemeButton.classList.contains(colorSchemeOn)) {
+            colorSchemeButton.classList.remove(colorSchemeOn);
+        }
+        rainbowButton.classList.add(rainbowOn);
+    }
+}
+
+function toggleColorSchemeMode () {
+    const rainbowOn = "rainbow-on";
+    const colorSchemeOn = "color-scheme-on";
+
+    const rainbowButton = document.querySelector(".rainbow-button");
+    const colorSchemeButton = document.querySelector(".color-scheme-button");
+
+    if (colorSchemeButton.classList.contains(colorSchemeOn)) {
+        colorSchemeButton.classList.remove(colorSchemeOn);
+    } else {
+        if(rainbowButton.classList.contains(rainbowOn)) {
+            rainbowButton.classList.remove(rainbowOn);
+        }
+        colorSchemeButton.classList.add(colorSchemeOn);
+    }
+}
+
+function shiftColor (hexColor) {
+    let r = hexToR(hexColor);
+    let g = hexToG(hexColor);
+    let b = hexToB(hexColor);
+
+    r = randomShift(r);
+    g = randomShift(g);
+    b = randomShift(b);
+
+    r = constrain(r);
+    g = constrain(g);
+    b = constrain(b);
+
+    hexColor = rgbToHex(r,g,b);
+    
+    return hexColor;
+}
+
+function randomShift (num) {
+    return num + (40*randomSign());
+}
+
+function randomSign() {
+    return Math.random() < 0.5 ? -1 : 1;
+}
+
+
+function constrain (num) {
+    return Math.max(0,Math.min(num,255));
+}
+
+function rgbToHex(r,g,b) {
+    return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+}
+
+function componentToHex(component) {
+    let hex = component.toString(16);
+    hex = hex.length === 1 ? "0" + hex : hex;
+    return hex;
+}
+
+function hexToR (hexColor) {
+    return parseInt(hexColor.substr(1,2), 16);
+}
+
+function hexToG (hexColor) {
+    return parseInt(hexColor.substr(3,2), 16);
+}
+
+function hexToB (hexColor) {
+    return parseInt(hexColor.substr(5,2), 16);
 }
